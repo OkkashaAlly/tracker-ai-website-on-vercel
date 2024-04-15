1e2b735bd6dd3b40a4745128717e133baea80a53
OkkashaAlly <steamnature006@gmail.com>
2024-04-16 00:47:07 +0300

    COMPONENTS/SECTIONS: HeroSection.tsx - mobile responsiveness implemented

--- app/components/sections/HeroSection.tsx
+++ app/components/sections/HeroSection.tsx
@@ -4,18 +4,18 @@ import Link from "next/link";
 const HeroSection = () => {
   return (
     // h-[calc(100vh-6rem)]
-    <section className="pt-20">
-      <div className="xl:w-[95%] 2xl:w-[90%] max-w-screen-2xl mx-auto flex px-6">
+    <section className="pt-10 md:pt-20">
+      <div className="xl:w-[95%] 2xl:w-[90%] max-w-screen-2xl mx-auto flex flex-col-reverse md:flex-row px-6">
         {/* left  */}
-        <div className="w-[60%]">
-          <small className="uppercase text-[20px]">
+        <div className="text-center md:text-start w-full md:w-[60%] mb-20">
+          <small className="uppercase text-[14px] md:text-[20px]">
             Transform Your Crypto Portfolio Tracking
           </small>
-          <h1 className="text-[54px] font-black leading-tight	">
+          <h1 className="text-[34px] md:text-[54px] font-black leading-tight mt-2">
             The ultimate solution for managing your multi-chain portfolio
             effortlessly
           </h1>
-          <p className="w-[80%] mt-6 mb-10">
+          <p className="w-full md:w-[80%] mt-6 mb-10">
             Tracker AI is the ultimate multi-chain portfolio tracking solution.
             Simply paste your wallet address into the Telegram bot, and
             you&rsquo;ll instantly access all the tokens within your wallet. The
